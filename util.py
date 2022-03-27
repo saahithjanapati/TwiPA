2b94d1fabb54cc5193143318abebe1b8717b1a45
sidlakkoju <siddharth.lakkoju@gmail.com>
2022-03-27 11:33:27 -0400

    added link for tweet and fixed loading and centerd profile picture

--- util.py
+++ util.py
@@ -2,5 +2,6 @@ def generate_time_to_tweet_dict(tweets):
     """"generate dictionary in new format to enable fast lookup for graph hovering"""
     new_dict = {}
     for tweet in tweets:
-        new_dict[str(tweet["date"])[0:-6]] = [tweet["content"], tweet["id"]]
+        new_dict[str(tweet["date"])[0:-6]] = [tweet["content"], tweet["id"], "https://twitter.com/twitter/status/" + str(tweet["id"])]
+       
     return new_dict

ab12d4e717fead58c33d98cbbaeb8468cb309f3f
Saahith <22772542+saahithjanapati@users.noreply.github.com>
2022-04-01 20:04:22 -0400

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-bearerHandler = "AAAAAAAAAAAAAAAAAAAAAOEvawEAAAAAkRiuf%2BJ1IILXLHutNpLXFbrdVQU%3DnjZ0OStAOGRBLpf2yI1EwNPyuflnigYRg1C1wtHo5aqgoK5rdY"
-
-
-consumer_key = "AYpXp29vtsP0Om70k25xtxM9G"
-consumer_secret = "nU6Ri00sW6MQWKPHYNJM0o0qJbgiDPUWlJJyoxFNHXeLsTqIUG"
-
-access_token = "1403873715348193281-FYPfWIeTvxNpTN0UVN5zkq8Onh0TSj"
-access_token_secret = "aaQXM081aqcBpJki8MbawSLuPzgPnFxmRQaXs11A05BTg"
